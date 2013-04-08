ed457b76d299647728d2c21160413c950d8b9f1e
Neil Renicker <neilrenicker@gmail.com>
2013-04-08 17:19:31 -0400

    add flexible containers and Chirp.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+$(window).load(function() {
+  var resizer = function () {
+    var tallest_h2 = 0;
+    $('.article-title h2').each(function () {
+      if ($(this).outerHeight() > tallest_h2 ) {
+        tallest_h2 = $(this).outerHeight();
+      }
+    });
+    $('.article-title').css({'min-height': tallest_h2});
+  };
 
+  resizer(); // set initially
+
+  $(window).on('resize', resizer); // adjust on browser resize
+
+
+});
