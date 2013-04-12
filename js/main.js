2af0a900a3e8dc613d347ccece4e2fd9e38b4031
Neil Renicker <neilrenicker@gmail.com>
2013-04-12 16:51:11 -0400

    code refactoring and bugfixes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-$(window).load(function() {
-  $('.tweets').css('display', 'block');
+$(function () {
+  Chirp({
+    target: 'tweets',
+    user:'daytoncleancode',
+    max:2,
+    templates: {
+      base: '<div class="chirp clearfix">{{tweets}}</div>',
+      tweet: '<div class="tweet-container"><p>"{{html}}"</p><span class="meta"><time>&#8211;<a href="http://twitter.com/{{user.screen_name}}/statuses/{{id_str}}">{{time_ago}}</a></time> via <a href="http://twitter.com/{{user.screen_name}}" title="{{user.name}} — {{user.description}}">@{{user.screen_name}}</a></span></div>'
+    },
+    success: function( data ){
+      $('.tweets').css('display', 'block');
+    }
+  })
+});
 
+$(window).load(function() {
   var resizer = function () {
     var tallest_h2 = 0;
-    $('.article-title h2').each(function () {
-      if ($(this).outerHeight() > tallest_h2 ) {
-        tallest_h2 = $(this).outerHeight();
+    $('#past article h2').each(function () {
+      if ($(this).innerHeight() > tallest_h2 ) {
+        tallest_h2 = $(this).innerHeight();
       }
     });
-    $('.article-title').css({'min-height': tallest_h2});
+    $('#past article h2').css({'min-height': tallest_h2});
   };
 
   resizer(); // set initially
